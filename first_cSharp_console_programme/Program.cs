1122566e48b866ac0c812f0eec5e08cbdbbe0605
Declan <declan.munroe@ics.ie>
2018-07-23 20:57:46 +0100

    Add loop and if to programe

--- first_cSharp_console_programme/Program.cs
+++ first_cSharp_console_programme/Program.cs
@@ -7,7 +7,18 @@ namespace first_cSharp_console_programme
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+             
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+            }
             Console.ReadLine();
+
         }
     }
 }
