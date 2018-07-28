a7dfbd778a7f76ffb08708c2f26f62666c428a43
Declan <declan.munroe@ics.ie>
2018-07-28 17:23:11 +0100

    Creating first class properties and methods

---
+++ CarShowroomClass/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Opel";
+            myCar.Model = "Astra";
+            myCar.Year = 2007;
+            myCar.Color = "Blue";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+            Console.ReadLine();
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 1000.0M;
+            return carValue;
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carvalue;
+
+            if (Year > 2006)
+            {
+                carvalue = 2000;
+            }
+            else
+            {
+                carvalue = 1000;
+            }
+
+            return carvalue;
+        }
+    }
+}
