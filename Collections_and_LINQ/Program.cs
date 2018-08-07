20eb1835f433e529de5a0b3c3ba8b623029773de
Declan Munroe <declan.munroe@ics.ie>
2018-08-07 16:48:09 +0100

    Using LINQ

--- Collections_and_LINQ/Program.cs
+++ Collections_and_LINQ/Program.cs
@@ -46,13 +46,55 @@ namespace Collections_and_LINQ
             string[] names = { "Declan", "Kevin", "Joe", "Annette" };
 
             // Collection initializer
-            List<Car> myList = new List<Car>()
+            List<Car> myCars = new List<Car>()
             {
-                new Car { Make = "Fiat", Model = "Punto", VIN = "dpo" },
-                new Car { Make = "Renault", Model = "Clio", VIN = "iasa" }
+                new Car() { VIN="A1", Make = "BMW", Model= "550i", StickerPrice=55000, Year=2009},
+                new Car() { VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=35000, Year=2010},
+                new Car() { VIN="C3", Make="BMW", Model = "745li", StickerPrice=75000, Year=2008},
+                new Car() { VIN="D4", Make="Ford", Model="Escape", StickerPrice=25000, Year=2008},
+                new Car() { VIN="E5", Make="BMW", Model="55i", StickerPrice=57000, Year=2010}
             };
             ////////////////////////
-            
+
+            // LINQ query
+            var bmws = from car in myCars where car.Make == "BMW" select car;
+            Console.WriteLine("The results of the LINQ query");
+            foreach (var car in bmws)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            }
+
+            var carsDecending = from car in myCars orderby car.Year descending select car;
+            Console.WriteLine("The results of the LINQ query with decending");
+            foreach (var car in carsDecending)
+            {
+                Console.WriteLine(car.Year);
+            }
+
+            // LINQ method
+            var fords = myCars.Where(p => p.Make == "Ford" && p.Year == 2008); // we use var because we dont know what data type is been returned from the query
+            Console.WriteLine("The results of the LINQ method");
+            foreach (var car in fords)
+            {
+                Console.WriteLine("{0} {1}", car.Model, car.VIN);
+            }
+
+            var carsDecendingV2 = myCars.OrderByDescending(p => p.Year); // we use var because we dont know what data type is been returned from the query
+            Console.WriteLine("The results of the LINQ method with decending");
+            foreach (var car in carsDecendingV2)
+            {
+                Console.WriteLine(car.Year);
+            }
+
+            Console.WriteLine("Print out the first BMW");
+            var firstBmw = myCars.OrderByDescending(p => p.Year).First(p => p.Make == "BMW");
+            Console.WriteLine("The first bmw is : " + firstBmw.Year);
+
+            Console.WriteLine("True false query method");
+            Console.WriteLine(myCars.TrueForAll(p => p.Year > 2007));
+
+            Console.WriteLine("An example of a one line foreach statement with a LINQ method");
+            myCars.ForEach(p => Console.WriteLine("{0} {1:C}", p.VIN, p.StickerPrice));
 
 
             Console.ReadLine();
@@ -64,6 +106,8 @@ namespace Collections_and_LINQ
         public string Make { get; set; }
         public string Model { get; set; }
         public string VIN { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
     }
 
     class Book
