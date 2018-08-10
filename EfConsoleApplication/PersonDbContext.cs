87f314f043e1b1f6c342a9de601575640365088f
Declan Munroe <declan.munroe@ics.ie>
2018-08-10 17:00:40 +0100

    Entity framework

---
+++ EfConsoleApplication/PersonDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EfConsoleApplication
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext() : base("name=EFpersonConn")
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
